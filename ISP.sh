8fd45ad571ba224840cb38522771b42faabdd737
NightCore_official <137006075+Nigth1Core@users.noreply.github.com>
2025-06-02 22:14:50 +0400

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -1,1 +1,9 @@
 ТОЛЬКО IP и SSH
+
+useradd sshuser -u 1010
+echo "sshuser:P@ssw0rd" | chpasswd
+usermod -aG wheel sshuser
+touch /etc/sudoers
+vim /etc/sudoers
+sshuser ALL=(ALL) NOPASSWD:ALL
+#Дальше ssh
