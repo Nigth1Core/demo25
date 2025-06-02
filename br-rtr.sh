850cbe3b674111158af9178ef51fc48541e04a47
NightCore_official <137006075+Nigth1Core@users.noreply.github.com>
2025-06-02 22:21:20 +0400

    Update br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -11,7 +11,7 @@ encapsulation untagged
 interface int0
 connect port te0 service-instance te0/int0
 interface int1
-description "to hq-srv"
+description "to br-srv"
 ip address 192.168.17.1/27
 port te1
 service-instance te1/int1
@@ -19,16 +19,19 @@ encapsulation untagged
 interface int1
 connect port te1 service-instance te1/int1
 exit
+
 ip route 0.0.0.0/0 172.16.5.1
 username net_admin
 password P@$$word
 role admin
 exit
+
 int tunnel.0
 ip add 172.16.0.2/30
 ip mtu 1400
 ip tunnel 172.16.5.14 172.16.4.14 mode gre
 exit
+
 router ospf 1
 router-id 2.2.2.2
 network 172.16.0.0/30 area 0
@@ -36,15 +39,18 @@ network 192.168.17.0/27 area 0
 passive-interface default
 no passive-interface tunnel.0
 exit
+
 int tunnel.0
 ip ospf authentication message-digest
 ip ospf message-digest-key 1 md5 P@ssw0rd
 exit
+
 int int1
 ip nat inside
 int int0
 ip nat outside
 exit
+
 ip nat pool NAT_POOL 192.168.17.1-192.168.17.30
 ip nat source dynamic inside-to-outside pool NAT_POOL overload interface int0
 ntp timezone utc+4
