8462f7379db78f52dc42aa43ed7c63e48568099d
NightCore_official <137006075+Nigth1Core@users.noreply.github.com>
2025-06-02 21:20:57 +0400

    Update HQ-CLI.sh

--- HQ-CLI.sh
+++ HQ-CLI.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-
 hostnamectl set-hostname hq-cli.au-team.irpo;
+apt-get update && apt-get install tzdata  
+timedatectl set-timezone Europe/Samara
 
+# SSH user
 useradd sshuser -u 1010
 echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
@@ -9,25 +10,11 @@ touch /etc/sudoers
 vim /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
 
-CONFIG_FILE="/etc/openssh/sshd_config"
+#Дальше ssh
 
-echo "AllowUsers sshuser" | tee -a /etc/openssh/sshd_config
-awk -i inplace '/^#?Port[[:space:]]+22$/ {sub(/^#/,""); sub(/22/,"2024"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?MaxAuthTries[[:space:]]+6$/ {sub(/^#/,""); sub(/6/,"2"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PasswordAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-awk -i inplace '/^#?PubkeyAuthentication[[:space:]]+(yes|no)$/ {sub(/^#/,""); sub(/no/,"yes"); print; next} {print}' "$CONFIG_FILE"
-
-touch /etc/openssh/banner 
-cat <<EOF > /etc/openssh/banner
-Authorized access only  
-EOF  
-systemctl restart sshd  
-
+# А это к Samba  2.1 Syshelper
 echo "default_realm = AU-TEAM.IRPO" | sudo tee -a /etc/krb5.conf
 echo "nameserver 192.168.0.30" | sudo tee -a /etc/resolv.conf
-
-apt-get update && apt-get install tzdata  
-timedatectl set-timezone Europe/Samara
 
 apt-get update && apt-get install -y gpupdate
 gpupdate-setup enable
