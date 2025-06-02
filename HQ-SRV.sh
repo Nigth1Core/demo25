2069a2df3d89b444f577c2416bca3a372f50ec90
NightCore_official <137006075+Nigth1Core@users.noreply.github.com>
2025-06-02 22:12:59 +0400

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -7,70 +7,18 @@ usermod -aG wheel sshuser
 touch /etc/sudoers
 vim /etc/sudoers
 sshuser ALL=(ALL) NOPASSWD:ALL
-
+#Дальше ssh
 
 apt-get update && apt-get install tzdata  
 timedatectl set-timezone Europe/Samara
 
-
-
-apt-get install -y chrony dnsmasq
-cat <<EOF > /etc/chrony.conf
-# Use public servers from the pool.ntp.org project.
-# Please consider joining the pool (https://www.pool.ntp.org/join.html).
-#pool pool.ntp.org iburst
-server 192.168.1.62 iburst prefer
-# Record the rate at which the system clock gains/losses time.
-driftfile /var/lib/chrony/drift
-
-# Allow the system clock to be stepped in the first three updates
-# if its offset is larger than 1 second.
-makestep 1.0 3
-
-# Enable kernel synchronization of the real-time clock (RTC).
-rtcsync
-
-# Enable hardware timestamping on all interfaces that support it.
-#hwtimestamp *
-
-# Increase the minimum number of selectable sources required to adjust
-# the system clock.
-#minsources 2
-
-# Allow NTP client access from local network.
-#allow 192.168.0.0/16
-
-# Serve time even if not synchronized to a time source.
-#local stratum 10
-
-# Require authentication (nts or key option) for all NTP sources.
-#authselectmode require
-
-# Specify file containing keys for NTP authentication.
-#keyfile /etc/chrony.keys
-
-# Save NTS keys and cookies.
-ntsdumpdir /var/lib/chrony
-
-# Insert/delete leap seconds by slewing instead of stepping.
-#leapsecmode slew
-
-# Get TAI-UTC offset and leap seconds from the system tz database.
-#leapsectz right/UTC
-
-# Specify directory for log files.
-logdir /var/log/chrony
-
-# Select which information is logged.
-#log measurements statistics tracking
-EOF
-
+# DNS
 apt-get update && apt-get install -y dnsmasq
-cat > /etc/dnsmasq.conf <<EOF
+vim /etc/dnsmasq.conf 
 no-resolv
 no-poll
 no-hosts
-listen-address=192.168.1.62
+listen-address=192.168.18.62
 
 server=77.88.8.8
 server=8.8.8.8
@@ -79,19 +27,15 @@ cache-size=1000
 all-servers
 no-negcache
 
-host-record=hq-rtr.au-team.irpo,192.168.1.1
-host-record=hq-srv.au-team.irpo,192.168.1.62
-host-record=hq-cli.au-team.irpo,192.168.1.66
+host-record=hq-rtr.au-team.irpo,192.168.18.1
+host-record=hq-srv.au-team.irpo,192.168.18.62
+host-record=hq-cli.au-team.irpo,192.168.18.66
 
-address=/br-rtr.au-team.irpo/192.168.0.1
-address=/br-srv.au-team.irpo/192.168.0.30
+address=/br-rtr.au-team.irpo/192.168.17.1
+address=/br-srv.au-team.irpo/192.168.17.30
 
 cname=moodle.au-team.irpo,hq-rtr.au-team.irpo
 cname=wiki.au-team.irpo,hq-rtr.au-team.irpo
-EOF
-systemctl restart dnsmasq
 
-grep -E "Port|MaxAuthTries|PasswordAuthentication|PubkeyAuthentication" "$CONFIG_FILE"
-cat /etc/dnsmasq.conf
-timedatectl
+systemctl restart dnsmasq
 
